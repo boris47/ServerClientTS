f59c4fcf7e5737553f0dc1d03226aef550ebf156
PC-ROBERTO\Roberto <leogranderoberto@gmail.com>
2020-02-12 16:24:19 +0100

    Removed old code from batches

--- Server/Setup_Server.bat
+++ Server/Setup_Server.bat
@@ -3,33 +3,6 @@
 REM SETUP FOR SERVER
 call	npm install
 
-if not exist ".vscode" (
-	mkdir ".vscode"
-)
-
-echo { 															> ".vscode/launch.json"
-echo 	"version": "0.2.0",										>> ".vscode/launch.json"
-echo 	"configurations": [                                     >> ".vscode/launch.json"
-echo 		{                                                   >> ".vscode/launch.json"
-echo 			"type": "node",                                 >> ".vscode/launch.json"
-echo 			"request": "launch",                            >> ".vscode/launch.json"
-echo 			"name": "Launch Program",                       >> ".vscode/launch.json"
-echo 			"program": "${workspaceFolder}\\Server.js",     >> ".vscode/launch.json"
-echo 			"outFiles": [                                   >> ".vscode/launch.json"
-echo 				"${workspaceFolder}/*.js"     	            >> ".vscode/launch.json"
-echo 			]												>> ".vscode/launch.json"
-echo 		}                                                   >> ".vscode/launch.json"
-echo 	]                                                       >> ".vscode/launch.json"
-echo } 															>> ".vscode/launch.json"
-
-echo { 															> tsconfig.json
-echo     "compilerOptions": { 									>> tsconfig.json
-echo         "module": "commonjs", 								>> tsconfig.json
-echo         "sourceMap": true, 								>> tsconfig.json
-echo         "watch": true 										>> tsconfig.json
-echo     } 														>> tsconfig.json
-echo } 															>> tsconfig.json
-
 call "./node_modules/.bin/tsc.cmd" -p tsconfig.json --watch false
 
 start "" /B code . -n -g Server.ts
