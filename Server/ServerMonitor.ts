125a64f418606138e8d7e6b2c5737d5983ed085a
boris47 <leogranderoberto@gmail.com>
2020-03-16 15:10:04 +0100

    Server is a forked process

--- Server/ServerMonitor.ts
+++ Server/ServerMonitor.ts
@@ -1,11 +1,23 @@
 
-import * as child_process from 'child_process';
 import * as fs from 'fs';
 import { ProcessContainer } from '../Common/ProcessContainer';
 
 
 if ( fs.existsSync( 'Server.js' ) )
 {
-	ProcessContainer.Execute( 'ServerTS', './Server.js' );
+	ProcessContainer.Fork.ForkProcess( 'ServerTS', './Server.js' )
+	.then( ( forked : ProcessContainer.Fork.ForkedProcess ) =>
+		{
+			forked.SendMessage( ProcessContainer.Fork.EForkMessageType.UPDATE,
+				( messsage : ProcessContainer.Fork.ISubProcessMessage ) =>
+				{
+					console.log( 'MONITOR received\n' + JSON.stringify( messsage, null, 4 ) );
+				},
+				() =>
+				{
+					console.log("MONITOR message 'update' not receive a response ");
+				}
+			);
+		}
+	);
 }
-
