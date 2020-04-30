2807897536f8b26e2f9a862bec881b7131e9c4bb
boris47 <leogranderoberto@gmail.com>
2020-04-30 20:59:14 +0200

    Added a direct client api test

---
+++ Client/src/main/client/Test.ts
@@ -0,0 +1,49 @@
+/*
+import { RequestServerPing,
+//	RequestFileUpload,
+//	RequestFileDownload,
+//	RequestGetData,
+//	RequestPutData
+} from "./client.RequestProcessor";
+
+import * as path from 'path';
+*/
+async function Main()
+{
+	{
+	/*	RequestServerPing()
+		.then( ( bCanContinue : boolean ) =>
+		{
+			return bCanContinue ? RequestFileUpload( path.join( process.cwd(), 'src/main/client/Test.js' ) ) : Promise.reject(false);
+		})
+	*/		
+	/*	.then( ( bCanContinue : boolean ) =>
+		{
+			return bCanContinue ? RequestFileDownload( './Server.js' ) : Promise.reject(false);
+		})
+	*/
+	/*	.then( ( bCanContinue : boolean ) =>
+		{
+			return bCanContinue ? RequestPutData( 'MyDataName', '123' ) : Promise.reject(false);
+		})
+	*/
+	/*	.then( ( bCanContinue : boolean ) =>
+		{
+			return bCanContinue ? RequestGetData<string>( 'MyDataName' ) : Promise.reject(null);
+		})
+	*/
+	/*	.then( ( value: string | null ) =>
+		{
+			console.log( "Value", value?.toString() );
+		})
+	*/
+	/*	.catch( reason => console.error(reason) );
+	*/	
+	}
+}
+
+Main();
+
+
+
+
