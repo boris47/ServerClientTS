c78ee1156e748a7dbc181aa5823bdbb23db54edd
boris47 <leogranderoberto@gmail.com>
2020-06-06 00:21:14 +0200

    Better progress spinner

--- Client/src/renderer/components/Progress/ProgressSpinner.vue
+++ Client/src/renderer/components/Progress/ProgressSpinner.vue
@@ -1,37 +1,79 @@
 <template>
-	<div width="120px" height="120px">
-		<div class="lds-dual-ring"></div>
-	</div>
+	<div class="dual-ring" :style="style"/>
 </template>
 
 <script lang="ts">
 
-import { Component, Vue } from 'vue-property-decorator';
+import { Component, Vue, Prop, Watch } from 'vue-property-decorator';
+import { PropOptions } from 'vue';
 
 @Component
 export default class ProgressSpinner extends Vue
 {
-	
+	/** Determine button to be disabled */
+	@Prop(<PropOptions<Number>>
+	{
+		required: false,		type: Number,
+		default: () => 64,
+		validator: (value: number) => value > 0,
+	})
+	protected side: number;
+
+	/** Determine button to be disabled */
+	@Prop(<PropOptions<Number>>
+	{
+		required: false,		type: Number,
+		default: () => 64,
+		validator: (value: number) => value > 0,
+	})
+	protected height: number;
+
+	protected style : string = '';
+
+
+	@Watch('side')
+	protected OnSideChanged( newValue: number )
+	{
+		console.log('watch')
+		this.side = newValue;
+		this.UpdateStyle();
+	}
+
+	protected mounted()
+	{
+		console.log('mounted')
+		this.UpdateStyle();
+	}
+
+	private UpdateStyle()
+	{
+		const factor = ( this.side / 64 );
+		const borderWidth = factor * 6;
+		const marginTop = factor * 3;
+		this.style = `
+			width: ${this.side}px;
+			height: ${this.side}px;
+			margin: ${marginTop}px;
+			border-width: ${borderWidth}px;
+		`;
+	}
 }
 
 </script>
 
 <style scoped>
 
-.lds-dual-ring {
-	display: inline-block;
-	width: 20px;
-	height: 20px;
-}
-.lds-dual-ring:after {
-	content: " ";
+.dual-ring {
+	content: "";
 	display: block;
-	width: 14px;
-	height: 14px;
+	width: 64px;
+	height: 64px;
+	margin-top: 3px;
 	border-radius: 50%;
-	border: 6px solid rgb(5, 165, 0);
-	border-color: rgb(5, 165, 0) transparent #fff transparent;
-	animation: lds-dual-ring 1.0s linear infinite;
+	border-width: 6px;
+	border-style: solid;
+	border-color: green transparent green transparent;
+	animation: lds-dual-ring 1.2s linear infinite;
 }
 @keyframes lds-dual-ring {
 	0% {
@@ -41,6 +83,5 @@ export default class ProgressSpinner extends Vue
 		transform: rotate(360deg);
 	}
 }
-
 
 </style>
