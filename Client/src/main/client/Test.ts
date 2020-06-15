188094870c9d40bdc437626f361fb707cb3decf6
boris47 <leogranderoberto@gmail.com>
2020-06-15 09:05:27 +0200

    Completely enabled test for client-server comunications

--- Client/src/main/client/Test.ts
+++ Client/src/main/client/Test.ts
@@ -2,10 +2,10 @@
 	import {
 		RequestServerPing,
 		RequestStorageList,
-	//	RequestResourceUpload,
-	//	RequestResourceDownload,
-	//	RequestGetData,
-	//	RequestPutData
+		RequestResourceUpload,
+		RequestResourceDownload,
+		RequestGetData,
+		RequestPutData
 	} from "./client";
 
 	import { InstallRequestsProcessor } from "./client";
@@ -22,33 +22,33 @@ async function Main()
 			return Buffer.isBuffer(result) ? RequestStorageList() : Promise.reject(result);
 		})
 	
-	/*	.then( ( result : Buffer | Error ) =>
+		.then( ( result : Buffer | Error ) =>
 		{
-			return Buffer.isBuffer(result) ? RequestResourceUpload( './exampleFile' ) : Promise.reject(result);
+			return Buffer.isBuffer(result) ? RequestResourceUpload( './Tests/RequestResourceUpload/GPU-Z.2.25.0.exe' ) : Promise.reject(result);
 		})
-	*/
-	/*	.then( ( result : Buffer | Error ) =>
+	
+		.then( ( result : Buffer | Error ) =>
 		{
-			return Buffer.isBuffer(result) ? RequestResourceDownload( 'exampleFile', 'E:/SourceTree/ServerClientTS/Client/Downloads' ) : Promise.reject(result);
+			return Buffer.isBuffer(result) ? RequestResourceDownload( 'GPU-Z.2.25.0.exe', 'E:/SourceTree/ServerClientTS/Client/Tests/RequestResourceDownload' ) : Promise.reject(result);
 		})
-	*/
-	/*	.then( ( result : Buffer | Error ) =>
+	
+		.then( ( result : Buffer | Error ) =>
 		{
 			return Buffer.isBuffer(result) ? RequestPutData( 'MyDataName', '123' ) : Promise.reject(result);
 		})
-	*/
-	/*	.then( ( result : Buffer | Error ) =>
+	
+		.then( ( result : Buffer | Error ) =>
 		{
 			return Buffer.isBuffer(result) ? RequestGetData( 'kedyy' ) : Promise.reject(result);
 		})
-	*/
-	/*	.then( ( value: string | null ) =>
+	
+		.then( ( value: Error | Buffer ) =>
 		{
-			console.log( "Value", value?.toString() );
+			console.log( "Value", value.toString() );
 		})
-	*/
-	/*	.catch( reason => console.error(reason) );
-	*/	
+	
+		.catch( reason => console.error(reason) );
+		
 	}
 }
 
