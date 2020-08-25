7a81924b75a0d7655ee0928798f4c03af0f527ad
boris47 <leogranderoberto@gmail.com>
2020-08-25 23:06:34 +0200

    No String utils function for time format

--- Common/Utils/StringUtils.ts
+++ Common/Utils/StringUtils.ts
@@ -1,13 +1,13 @@
 
 
-export class StringUtils {
+export default class StringUtils {
 	
 	/** Helper function to find the nth occurrence of a character in a string
 	 * @param str Source string
 	 * @param substring string to search for
 	 * @param nth starting index of search
 	 */
-	public static GetSubstringIndex( str : string, substring : string, nth : number )
+	public static GetSubstringIndex( str : string, substring : string, nth : number ): number
 	{
 		const l = str.length;
 		let i = -1;
@@ -18,5 +18,18 @@ export class StringUtils {
 			break;
 		}
 		return i++;
+	}
+	
+	/**
+	 * @param milliseconds 
+	 */
+	public static FormatTime(milliseconds: number): string
+	{
+		const date 			= new Date(milliseconds);
+		const hoursString   = date.getUTCHours().toString().padStart(2, '0');
+		const minutesString = date.getUTCMinutes().toString().padStart(2, '0');
+		const secondsString = date.getUTCSeconds().toString().padStart(2, '0');
+		const msString 		= date.getUTCMilliseconds().toString().padStart(4, '0');
+		return `${hoursString}:${minutesString}:${secondsString}.${msString}`;
 	}
 }
