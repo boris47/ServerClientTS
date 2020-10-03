a30a130084e15d9a4b586e5c9b24984e52cbc4e4
boris47 <leogranderoberto@gmail.com>
2020-10-04 00:34:58 +0200

    Fixed production build node require call

--- Client/src/main/preload.ts
+++ Client/src/main/preload.ts
@@ -3,29 +3,17 @@
 const nodeRequire = require;
 
 // In development mode we need the require function to get hot reload to work
-const allowedModules = ['module', 'source-map-support/source-map-support.js', 'vue-router', 'electron' ];
 window.require = <any>((requiredModule: string) =>
 {
-	console.log(`Required module "${ requiredModule }" (${ typeof (requiredModule) })`);
-	
-	if (!allowedModules.includes(requiredModule))
-	{
-		new Error(`Tried to require unknown module: ${ requiredModule }`);
-	}
-	else
-	{
-		return nodeRequire(requiredModule);
-	}
-
-/*	switch(requiredModule)
+	console.log(`Required module "${requiredModule}"`);
+	switch(requiredModule)
 	{
 		case 'module': return nodeRequire('module');
 		case "source-map-support/source-map-support.js": return nodeRequire("source-map-support/source-map-support.js");
 		case 'vue-router': return nodeRequire('vue-router');
 		case 'electron': return nodeRequire('electron');
-		default: new Error(`Tried to require unknown module: ${ requiredModule }`);
+		default: new Error(`Tried to require unknown module: ${requiredModule}`);
 	}
-*/
 });
 
 // Make a module available
